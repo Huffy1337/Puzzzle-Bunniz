f1843792d62422f2a10eedbeb0d0ff2b60f4ee1a
Korbinian Maag <korbinian.maag@gmail.com>
2017-02-26 17:50:21 +0100

    Generated Unity Project

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 objectPosition = transform.position;
+
+        //Move Horizontal
+        if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") > 0) 
+        {
+            objectPosition.x = transform.position.x + 320;
+            Debug.Log("Right");
+        } else if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") < 0) {
+            Debug.Log("Left");
+        }
+
+        //Move Vertical
+
+
+    }
+}
